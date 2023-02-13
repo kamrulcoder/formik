fd490d7e29535278f735df17eeb9f59066267257
kamrulcoder <kamrulcoder@gmail.com>
2023-02-13 20:56:26 +0600

    Formik  practice]

--- README.md
+++ README.md
@@ -16,170 +16,71 @@ Tailwind css install for React vite project
     npm install -D tailwindcss postcss autoprefixer
     npx tailwindcss init -p
 
-> ### Formik all Component List -
-
-    - Formik
-    - Form
-    - Field
-    - as="textarea' or component="any html element"
-
-#### Field Revisited
+#### Field Level Validation
 
 ```javascript
-<Field name="address">
-  {(props) => {
-    console.log(props);
+const onSubmit = (values, submitProps) => {
+  console.log("Form data", values);
+  console.log("submitProps", submitProps);
+};
+
+<Field
+  as="textarea"
+  // component="textarea"
+  className="w-full px-3 py-2  font-bold  bg-gray-300"
+  name="comments"
+  type="text"
+  placeholder="name"
+  validate={validateComments}
+/>;
+```
+
+#### Manually trigering validation
+
+```javascript
+<Formik
+  initialValues={initialValues}
+  validationSchema={validationSchema}
+  onSubmit={onSubmit}
+>
+  {(formik) => {
+    console.log(formik);
+
     return (
-      <input
-        type="text"
-        className="block  w-full bg-gray-200 px-3 py-2"
-        placeholder="address"
-      />
+      <Form>
+        <div className="flex items-center justify-center gap-10">
+          <button
+            type="button"
+            onClick={() => formik.validateField("comments")}
+          >
+            Validate comments
+          </button>
+          <button
+            type="button"
+            onClick={() => formik.setFieldTouched("comments")}
+          >
+            Visit comments
+          </button>
+          <button type="button" onClick={() => formik.validateForm()}>
+            Validate all
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              formik.setTouched({
+                name: true,
+                email: true,
+                channel: true,
+                comments: true,
+              })
+            }
+          >
+            Visit all
+          </button>
+
+        </div>
+      </Form>
     );
   }}
-</Field>
+</Formik>
 ```
-
-```javascript
-Object { field: {…}, form: {…}, meta: {…} }
-​
-field: Object { name: "address", value: "", onChange: useEventCallback()
-, … }
-​
-form: Object { isSubmitting: false, isValidating: false, submitCount: 0, … }
-​
-meta: Object { value: "", touched: false, initialTouched: false, … }
-```
-
-### ErrorMessage Revisited
-
-```javascript
-<ErrorMessage name="name" component="div" />
-
- <ErrorMessage  name="email">
-     {errMsg =>  <div  className="text-red-900"> {errMsg}</div>}
-  </ErrorMessage>
-  <ErrorMessage  name="name"  component={TextError}/>  // TextError Component
-```
-
-TextError.jsx
-
-```javascript
-import React from "react";
-
-function TextError({ children }) {
-  return <div className="text-red-900">{children}</div>;
-}
-
-export default TextError;
-```
-
-### Nested Objects
-
-```javascript
-  // intitial values declaire
-  const initialValues =  {
-    name: "",
-    email: "",
-    channel: "",
-    comments:"",
-    address:"",
-    social:{
-      fb:"",
-      ln:""
-    }
-  }
-
-
-    <div>
-      <label htmlFor="">FaceBook  : </label>
-        <Field
-          className="w-full px-3 py-2  font-bold  bg-gray-300"
-          name="social.fb"
-          type="text"
-          placeholder="social.fb" />
-        {/* <ErrorMessage  name="social.ln"/> */}
-      </div>
-      <div>
-        <label htmlFor="">Linkedin   : </label>
-        <Field
-          className="w-full px-3 py-2  font-bold  bg-gray-300"
-          name="social.ln"
-          type="text"
-          placeholder="Linkedin" />
-        {/* <ErrorMessage  name="social.ln"/> */}
-      </div>
-
-```
-
-### - Arrays
-
-```javascript
-  const initialValues = {
-    phoneNumber:['','']
-  };
-
-   <div>
-      <label htmlFor="">Phone Number: </label>
-      <div className="flex items-center  gap-4 ">
-      <Field
-          className="w-1/2 px-3 py-2  font-bold  bg-gray-300"
-          name="phoneNumber[0]"
-          type="text"
-          placeholder="Linkedin"
-        />
-        <Field
-        className="w-1/2 px-3 py-2  font-bold  bg-gray-300"
-        name="phoneNumber[1]"
-        type="text"
-        placeholder="Linkedin"
-      />
-      </div>
-
-```
-
-### FieldArray
-
-```javascript
-  const initialValues = {
-    phNumbers:['']
-  };
-
-
-<FieldArray name="phNumbers">
-  {(fieldArrayProps) => {
-    const { push, remove, form } = fieldArrayProps;
-    const { values } = form;
-    const { phNumbers } = values;
-    return (
-      <div>
-        {phNumbers.map((number, ind) => (
-          <div key={ind}>
-            <Field
-              type="text"
-              className="bg-gray-100 px-3 py-2 my-2"
-              name={`phNumbers[${ind}]`}
-            />
-            {ind > 0 && (
-              <button onClick={() => remove(ind)} className="mx-2">
-                Remove
-              </button>
-            )}
-
-            <button type="button" onClick={() => push("")}>
-              Add
-            </button>
-          </div>
-        ))}
-      </div>
-    );
-  }}
-</FieldArray>
-```
-
-
-
-
-#### Diffrent Field and FirstField 
-একটি ফর্ম এ  একাধিক ইনপুট  ফর্ম থাকে ।  যদি Field  ব্যবহার করা হয় তাহলে অন্য ইনপুট এ Event  ঘটলেও সকল ইনপুট এ   Render  হয় ।  যা Big Problem 
-কিন্তু  FirsField  এ অন্য ইনপুট এ Event  ঘটলে তা  Render   হয় না । 
--- src/components/FormikForm.jsx
+++ src/components/FormikForm.jsx
@@ -18,8 +18,7 @@ function FormikForm() {
       ln: "",
     },
     phoneNumber: ["", ""],
-    phNumbers: ['']
-
+    phNumbers: [""],
   };
 
   // validation scheema of yup
@@ -27,34 +26,16 @@ function FormikForm() {
     name: Yup.string().required("Name field is required "),
     email: Yup.string().required("email required").email("invalid email"),
     channel: Yup.string().required("Channek  field is required "),
-    comments: Yup.string().required("Required"),
     address: Yup.string().required("Required"),
   });
 
-  // validation form
-  const validate = (values) => {
-    let errors = {};
-
-    // name validate input
-    if (!values.name) {
-      errors.name = "Required Name ";
+  // comment validate
+  const validateComments = (value) => {
+    let error;
+    if (!value) {
+      error = "Command Field Required! ";
     }
-
-    // email validate input
-    if (!values.email) {
-      errors.email = "Required Name ";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email format";
-    }
-
-    // channel   input validate
-    if (!values.channel) {
-      errors.channel = "Required Channel ";
-    }
-
-    return errors;
+    return error;
   };
 
   const onSubmit = (values, submitProps) => {
@@ -62,151 +43,186 @@ function FormikForm() {
     console.log("submitProps", submitProps);
   };
 
-  // // second declaire formik
-  // const formik = useFormik({
-  //   // initial value
-  //   initialValues,
-  //   // onsubmit value  reuturn
-  //   onSubmit,
-  //   // validate
-  //   validationSchema
-  // });
-
   return (
     <div className="w-[600px]   mx-auto    shadow-lg p-10">
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={onSubmit}
+        validateOnMount
       >
-        <Form className="flex flex-col gap-5">
-          <div>
-            <label htmlFor="">Name : </label>
-            <Field
-              className="w-full px-3 py-2  font-bold  bg-gray-300"
-              name="name"
-              type="text"
-              placeholder="name"
-            />
-            <ErrorMessage name="name" component={TextError} />
-          </div>
-          <div>
-            <label htmlFor="">Email : </label>
-            <Field
-              className="w-full px-3 py-2  font-bold  bg-gray-300"
-              name="email"
-              type="text"
-              placeholder="email"
-            />
-            <ErrorMessage name="email">
-              {(errMsg) => <div className="text-red-900"> {errMsg}</div>}
-            </ErrorMessage>
-          </div>
-          <div>
-            <label htmlFor="">Channel : </label>
-            <Field
-              className="w-full px-3 py-2  font-bold  bg-gray-300"
-              name="channel"
-              type="text"
-              placeholder="channel"
-            />
-            <ErrorMessage name="channel" />
-          </div>
-          <div>
-            <label htmlFor="">Comments : </label>
-            <Field
-              as="textarea"
-              // component="textarea"
-              className="w-full px-3 py-2  font-bold  bg-gray-300"
-              name="comments"
-              type="text"
-              placeholder="name"
-            />
-            <ErrorMessage name="comments" />
-          </div>
+        {(formik) => {
+          console.log(formik);
 
-          <div>
-            <label htmlFor="address">Address</label>
-            <Field name="address">
-              {({ field, form, meta }) => {
-                // console.log('Field render')
-                return (
-                  <div>
-                    <input
-                      type="text"
-                      className="w-full bg-gray-200 px-3 py-2"
-                      {...field}
-                    />
-                    {meta.touched && meta.error ? (
-                      <div>{meta.error}</div>
-                    ) : null}
-                  </div>
-                );
-              }}
-            </Field>
-          </div>
-          <div>
-            <label htmlFor="">FaceBook : </label>
-            <Field
-              className="w-full px-3 py-2  font-bold  bg-gray-300"
-              name="social.fb"
-              type="text"
-              placeholder="social.fb"
-            />
-            {/* <ErrorMessage  name="social.ln"/> */}
-          </div>
-          <div>
-            <label htmlFor="">Phone Number: </label>
-            <div className="flex items-center  gap-4 ">
-              <Field
-                className="w-1/2 px-3 py-2  font-bold  bg-gray-300"
-                name="phoneNumber[0]"
-                type="text"
-                placeholder="Linkedin"
-              />
-              <Field
-                className="w-1/2 px-3 py-2  font-bold  bg-gray-300"
-                name="phoneNumber[1]"
-                type="text"
-                placeholder="Linkedin"
-              />
-            </div>
+          return (
+            <Form className="flex flex-col gap-5">
+              <div>
+                <label htmlFor="">Name : </label>
+                <Field
+                  className="w-full px-3 py-2  font-bold  bg-gray-300"
+                  name="name"
+                  type="text"
+                  placeholder="name"
+                />
+                <ErrorMessage name="name" component={TextError} />
+              </div>
+              <div>
+                <label htmlFor="">Email : </label>
+                <Field
+                  className="w-full px-3 py-2  font-bold  bg-gray-300"
+                  name="email"
+                  type="text"
+                  placeholder="email"
+                />
+                <ErrorMessage name="email">
+                  {(errMsg) => <div className="text-red-900"> {errMsg}</div>}
+                </ErrorMessage>
+              </div>
+              <div>
+                <label htmlFor="">Channel : </label>
+                <Field
+                  className="w-full px-3 py-2  font-bold  bg-gray-300"
+                  name="channel"
+                  type="text"
+                  placeholder="channel"
+                />
+                <ErrorMessage name="channel" />
+              </div>
+              <div>
+                <label htmlFor="">Comments : </label>
+                <Field
+                  as="textarea"
+                  // component="textarea"
+                  className="w-full px-3 py-2  font-bold  bg-gray-300"
+                  name="comments"
+                  type="text"
+                  placeholder="name"
+                  validate={validateComments}
+                />
+                <ErrorMessage name="comments" />
+              </div>
 
-            {/* <ErrorMessage  name="social.ln"/> */}
-          </div>
+              <div>
+                <label htmlFor="address">Address</label>
+                <Field name="address">
+                  {({ field, form, meta }) => {
+                    // console.log('Field render')
+                    return (
+                      <div>
+                        <input
+                          type="text"
+                          className="w-full bg-gray-200 px-3 py-2"
+                          {...field}
+                        />
+                        {meta.touched && meta.error ? (
+                          <div>{meta.error}</div>
+                        ) : null}
+                      </div>
+                    );
+                  }}
+                </Field>
+              </div>
+              <div>
+                <label htmlFor="">FaceBook : </label>
+                <Field
+                  className="w-full px-3 py-2  font-bold  bg-gray-300"
+                  name="social.fb"
+                  type="text"
+                  placeholder="social.fb"
+                />
+                {/* <ErrorMessage  name="social.ln"/> */}
+              </div>
+              <div>
+                <label htmlFor="">Phone Number: </label>
+                <div className="flex items-center  gap-4 ">
+                  <Field
+                    className="w-1/2 px-3 py-2  font-bold  bg-gray-300"
+                    name="phoneNumber[0]"
+                    type="text"
+                    placeholder="Linkedin"
+                  />
+                  <Field
+                    className="w-1/2 px-3 py-2  font-bold  bg-gray-300"
+                    name="phoneNumber[1]"
+                    type="text"
+                    placeholder="Linkedin"
+                  />
+                </div>
 
-          <div>
-            <label htmlFor="address">Phone Numbers </label>
-            <FieldArray name="phNumbers">
-              {(fieldArrayProps) => {
-                const { push, remove, form } = fieldArrayProps;
-                const {values } = form; 
-                const {phNumbers}  = values;
-                return (
-                  <div>
-                      {phNumbers.map((number, ind) => (
-                        <div  key={ind}>
-                          <Field   type="text"  className="bg-gray-100 px-3 py-2 my-2" name={`phNumbers[${ind}]`}/>
-                          {ind > 0 && <button  onClick={() => remove(ind)}  className="mx-2">Remove</button>}
-                          
-                          <button type="button"  onClick={() => push('')}>Add</button>
-                        </div>
-                      ))}
-                  </div>
-                )
-              }}
-            </FieldArray>
-          </div>
+                {/* <ErrorMessage  name="social.ln"/> */}
+              </div>
 
-          <div className="text-right">
-            <button
-              type="submit"
-              className="bg-blue-200  rounded-lg px-3 py-2 font-bold"
-            >
-              Submit
-            </button>
-          </div>
-        </Form>
+              <div>
+                <label htmlFor="address">Phone Numbers </label>
+                <FieldArray name="phNumbers">
+                  {(fieldArrayProps) => {
+                    const { push, remove, form } = fieldArrayProps;
+                    const { values } = form;
+                    const { phNumbers } = values;
+                    return (
+                      <div>
+                        {phNumbers.map((number, ind) => (
+                          <div key={ind}>
+                            <Field
+                              type="text"
+                              className="bg-gray-100 px-3 py-2 my-2"
+                              name={`phNumbers[${ind}]`}
+                            />
+                            {ind > 0 && (
+                              <button
+                                onClick={() => remove(ind)}
+                                className="mx-2"
+                              >
+                                Remove
+                              </button>
+                            )}
+
+                            <button type="button" onClick={() => push("")}>
+                              Add
+                            </button>
+                          </div>
+                        ))}
+                      </div>
+                    );
+                  }}
+                </FieldArray>
+              </div>
+
+              <div className="flex items-center justify-center gap-10">
+                {/* <button
+                  type="button"
+                  onClick={() => formik.validateField("comments")}
+                >
+                  Validate comments
+                </button>
+                <button
+                  type="button"
+                  onClick={() => formik.setFieldTouched("comments")}
+                >
+                  Visit comments
+                </button>
+                <button type="button" onClick={() => formik.validateForm()}>
+                  Validate all
+                </button>
+                <button
+                  type="button"
+                  onClick={() =>
+                    formik.setTouched({
+                      name: true,
+                      email: true,
+                      channel: true,
+                      comments: true,
+                    })
+                  }
+                >
+                  Visit all
+                </button> */}
+
+                <button type="submit"   disabled={!formik.isValid}> Submit</button>
+              </div>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
